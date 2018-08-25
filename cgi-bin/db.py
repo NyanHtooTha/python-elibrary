cfaca88fb0478c9d7ce40f8b58f5fec2a8b0600a
Nyan Htoo Tha <nyanhtootha@gmail.com>
2018-08-25 05:44:03 -0700

    fix bug on db.py

--- cgi-bin/db.py
+++ cgi-bin/db.py
@@ -9,8 +9,8 @@ cursor = connection.cursor()
        
 def create_tables(book_types):
     for each_type in book_types:
-        cursor.execute('create table %s (id integer primary key autoincrement unique not null, the_book text not null, read_count integer not null, read_date date not null)' %each_type)
-        cursor.execute('create index idx%s on %s (the_book, read_count, read_date)' %(each_type, each_type))
+        cursor.execute('create table "%s" (id integer primary key autoincrement unique not null, the_book text not null, read_count integer not null, read_date date not null)' %each_type)
+        cursor.execute('create index "idx%s" on "%s" (the_book, read_count, read_date)' %(each_type.replace(' ',''), each_type))
         connection.commit()
         
 def check_for_update(book_types):
@@ -23,16 +23,16 @@ def check_for_update(book_types):
         pass
 
 def insert_update_data(table_name, the_book):
-    check = cursor.execute('select * from %s where the_book="%s"' %(table_name, the_book)).fetchall()
+    check = cursor.execute('select * from "%s" where the_book="%s"' %(table_name, the_book)).fetchall()
     if not check:
-        cursor.execute('insert into %s(the_book, read_count, read_date) values (?, ?, ?)' %table_name, (the_book, 1, datetime.now())) 
+        cursor.execute('insert into "%s"(the_book, read_count, read_date) values (?, ?, ?)' %table_name, (the_book, 1, datetime.now())) 
     else:
-        cursor.execute('update %s set read_count=read_count+1, read_date=? where the_book=?', (table_name, datetime.now(), the_book))
+        cursor.execute('update "%s" set read_count=read_count+1, read_date=? where the_book=?' %table_name, (datetime.now(), the_book))
     connection.commit()
 
 #Core Algorithm SQL Query        
 def get_recommend_books(book_type):
-    raw_books = cursor.execute('select the_book from %s order by read_count desc, read_date desc limit 6' %book_type).fetchall()
+    raw_books = cursor.execute('select the_book from "%s" order by read_count desc, read_date desc limit 6' %book_type).fetchall()
     if raw_books:
         books = [book[0] for book in raw_books]
         return books
