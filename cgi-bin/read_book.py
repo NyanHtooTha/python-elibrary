be10206632765c39b708c3d7c8c6a28996f3a142
Nyan Htoo Tha <nyanhtootha@gmail.com>
2018-08-26 05:46:45 -0700

    handle book_to_read not to duplicate in db

--- cgi-bin/read_book.py
+++ cgi-bin/read_book.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 
 import cgi
+import cgitb
 import view
 import db
 
+#import sys
+
+cgitb.enable()
 form_data = cgi.FieldStorage()
 book_to_read = form_data['bookname'].value.replace('.jpg', '.pdf')
+if '\\' in book_to_read:
+    book_to_read = book_to_read.replace('\\', '/')
+#print('~~~', book_to_read, file=sys.stderr)
 #form_data.__del__()                        
 choiced_book_type = book_to_read.split('\\')[0][1:].split('/')[1]
 #images = glob.glob(book_to_read.split('\\')[0][1:]+'/*.jpg')
