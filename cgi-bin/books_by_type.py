5d6b03a12d63309a19cda91cb4f23ae62360ed34
Nyan Htoo Tha <nyanhtootha@gmail.com>
2018-08-26 04:55:37 -0700

    comment form_data.__del__()s

--- cgi-bin/books_by_type.py
+++ cgi-bin/books_by_type.py
@@ -6,7 +6,7 @@ import view
 
 form_data = cgi.FieldStorage()
 choiced_book_type = form_data['booktype'].value
-form_data.__del__()
+#form_data.__del__()
 images = glob.glob('books/'+choiced_book_type+'/*.jpg')
 view.__books_by_type__(choiced_book_type, images)
 
--- cgi-bin/read_book.py
+++ cgi-bin/read_book.py
@@ -6,7 +6,7 @@ import db
 
 form_data = cgi.FieldStorage()
 book_to_read = form_data['bookname'].value.replace('.jpg', '.pdf')
-form_data.__del__()                        
+#form_data.__del__()                        
 choiced_book_type = book_to_read.split('\\')[0][1:].split('/')[1]
 #images = glob.glob(book_to_read.split('\\')[0][1:]+'/*.jpg')
 db.insert_update_data(choiced_book_type, book_to_read)
